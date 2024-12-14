d49cdadb0e91dacaa8523673a527a12edaf2327d
Tac Tacelosky <tacman@gmail.com>
2024-12-14 15:12:48 -0500

    tweak label

--- bin/load-data.sh
+++ bin/load-data.sh
@@ -4,7 +4,7 @@ rm -f var/data.db & bin/console d:sch:update --force
 bin/console app:create-project omar es \
   --description="Omar Medina artista visual especializado en laca y cultura chiapaneca nos presenta una exposición basada en el uso de madera, piedra y barro como parte de su inspiración y armonía, dando paso a la creación de figuras y texturas azarosas e improvisada" \
   --label="Omar Expo" \
-  --labelFooter="Omar Medina, Sueño Aleteandoc"
+  --labelFooter="Omar Medina"
   --googleId="https://docs.google.com/spreadsheets/d/15VRctolDKQfkV5s9i_3QKg-cSGv2Edu07W28302qYyE/edit?gid=0#gid=0" \
   -vvv
 
